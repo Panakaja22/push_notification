cc3b3791a8e0660306a905265e5187fd0485525b
Pankaja22 <pankajapoojary22@gmail.com>
2023-10-03 16:00:19 +0530

    fix

--- push.js
+++ push.js
@@ -1,12 +1,11 @@
 // Generate vapIDKeys by running the command `node push.js`
-
-var push = require('web-push')
+const push = require('web-push')
 
 // let vapIdKeys = push.generateVAPIDKeys();
 
 // console.log(vapIdKeys)
 
-let vapidKeys = {
+const vapidKeys = {
     publicKey:'BKBolRC4aiE-YLXYuY8BLkiuBB4JpWQY-ZNBmQfko7_bBbK3By2LS1jyXDaDJ5MpldqtBZDIdwmDZB4DkFH9nsI',
     privateKey:'38YT3mNM3hFrr6bc4iy_Iy7HKVUgN6SOZGayvJRrU98'
 }
@@ -14,7 +13,7 @@ let vapidKeys = {
 push.setVapidDetails('mailto:test@code.co.uk',vapidKeys.publicKey,vapidKeys.privateKey)
 
 
-let sub = {
+const sub = {
     endpoint:"https://fcm.googleapis.com/fcm/send/dFhPz4YLt-I:APA91bHKOPB1qHGLtHIs5S52JSWPble_O95qAtfiQ0zKIpa1GCaSuOw9x13Gv5PA5h7aZlwlTgBisH9cOyW4pt6BbiTd2bsUOJiT16zqAB2ZT2eR1qUx4uJ3x3FFyOofy4lbtaEeu6vk",
 expirationTime:null,
 keys:
