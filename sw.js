b9bc9bb7a1d8e935639651bd262a4b248626c852
Pankaja22 <pankajapoojary22@gmail.com>
2023-10-03 10:51:14 +0530

    fix

--- sw.js
+++ sw.js
@@ -1,3 +1,29 @@
-self.addEventListener('push',()=>{
-    self.ServiceWorkerRegistration.sendNotification('test message',{})
-});
+// self.addEventListener('push',()=>{
+//     self.ServiceWorkerRegistration.sendNotification('test message',{})
+// });
+
+self.addEventListener('push',function(e){
+    var options = {
+        body:"This notification was generated from push!!",
+        icon:"images/example.png",
+        vibrate:[100,50,100],
+        date:{
+            dateOfArrival: Date.now(),
+            primaryKey:'2'
+        },
+        actions:[
+        {action:'explore',
+            title:'Explore this new world',
+            icon:"images/checkmark.png"
+        },
+        {action:'close',
+        title:'Close',
+        icon:"images/mark.png"
+    }
+        ]
+    };
+    e.waitUntil(
+        self.ServiceWorkerRegistration.showNotification('Hello world!',options)
+    );
+
+})
